3ae09a7eee2eecd44873275b9d0c088245883ce6
megha03-coder <meghachauhan310@gmail.com>
2025-08-14 18:00:04 +0530

    add sidebar

--- todo-app-assignment/src/App.jsx
+++ todo-app-assignment/src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './pages/Todo';
+import Sidebar from './components/Todo/Sidebar';
 
 export default function App() {
+  const [activeView, setActiveView] = useState('add') // Default to 'add' view
+
+  const handleViewChange = (view) => {
+    setActiveView(view)
+  }
+
   return (
     <div className="app">
       <header className="app-header">
         <h1>Simple Todo App</h1>
       </header>
       <main className="app-main">
-        <Todo />
+        <div className="sidebar-container">
+          <Sidebar activeView={activeView} onViewChange={handleViewChange} />
+        </div>
+        <div className="content-container">
+          <Todo activeView={activeView} />
+        </div>
       </main>
     </div>
   )
